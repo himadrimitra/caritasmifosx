e09b5dc8141a3e4999f5d7efbae70d83b37770cd
venkatconflux <venkata.conflux@confluxtechnologies.com>
2016-11-03 11:21:41 +0630

    2265 : resolving scheduler jobs failure issue

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/jobs/service/SchedulerJobListener.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/jobs/service/SchedulerJobListener.java
@@ -19,6 +19,7 @@
 package org.apache.fineract.infrastructure.jobs.service;
 
 import java.util.Date;
+import java.util.Random;
 
 import org.apache.fineract.infrastructure.core.service.ThreadLocalContextUtil;
 import org.apache.fineract.infrastructure.jobs.domain.ScheduledJobDetail;
@@ -30,6 +31,8 @@ import org.quartz.JobExecutionException;
 import org.quartz.JobKey;
 import org.quartz.JobListener;
 import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -45,6 +48,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SchedulerJobListener implements JobListener {
+	
+	 private final static Logger logger = LoggerFactory.getLogger(SchedulerJobListener.class);
 
     private int stackTraceLevel = 0;
 
@@ -82,50 +87,73 @@ public class SchedulerJobListener implements JobListener {
     }
 
     @Override
-    public void jobWasExecuted(final JobExecutionContext context, final JobExecutionException jobException) {
-        final Trigger trigger = context.getTrigger();
-        final JobKey key = context.getJobDetail().getKey();
-        final String jobKey = key.getName() + SchedulerServiceConstants.JOB_KEY_SEPERATOR + key.getGroup();
-        final ScheduledJobDetail scheduledJobDetails = this.schedularService.findByJobKey(jobKey);
-        final Long version = this.schedularService.fetchMaxVersionBy(jobKey) + 1;
-        String status = SchedulerServiceConstants.STATUS_SUCCESS;
-        String errorMessage = null;
-        String errorLog = null;
-        if (jobException != null) {
-            status = SchedulerServiceConstants.STATUS_FAILED;
-            this.stackTraceLevel = 0;
-            final Throwable throwable = getCauseFromException(jobException);
-            this.stackTraceLevel = 0;
-            StackTraceElement[] stackTraceElements = null;
-            errorMessage = throwable.getMessage();
-            stackTraceElements = throwable.getStackTrace();
-            final StringBuffer sb = new StringBuffer(throwable.toString());
-            for (final StackTraceElement element : stackTraceElements) {
-                sb.append("\n \t at ").append(element.getClassName()).append(".").append(element.getMethodName()).append("(")
-                        .append(element.getLineNumber()).append(")");
-            }
-            errorLog = sb.toString();
+	public void jobWasExecuted(final JobExecutionContext context, final JobExecutionException jobException) {
+		try {
+			final Trigger trigger = context.getTrigger();
+			final JobKey key = context.getJobDetail().getKey();
+			final String jobKey = key.getName() + SchedulerServiceConstants.JOB_KEY_SEPERATOR + key.getGroup();
+			ScheduledJobDetail scheduledJobDetails = null;
+			Integer maxIntervalBetweenRetries = ThreadLocalContextUtil.getTenant().getConnection()
+					.getMaxIntervalBetweenRetries();
+			Integer numberOfRetries = 0;
+			Integer maxNumberOfRetries = ThreadLocalContextUtil.getTenant().getConnection().getMaxRetriesOnDeadlock();
+			while (numberOfRetries <= maxNumberOfRetries) {
+			try {
+				numberOfRetries = numberOfRetries + 1;
+				scheduledJobDetails = this.schedularService.findByJobKey(jobKey);
+			} catch (Exception exception) {
+				logger.debug("finding job by job key failed for : " + jobKey + "with error :" + exception.getMessage());
+				try {
+						Random random = new Random();
+						int randomNum = random.nextInt(maxIntervalBetweenRetries + 1);
+						Thread.sleep(1000 + (randomNum * 1000));
+					}catch (InterruptedException e) {
 
-        }
-        String triggerType = SchedulerServiceConstants.TRIGGER_TYPE_CRON;
-        if (context.getMergedJobDataMap().containsKey(SchedulerServiceConstants.TRIGGER_TYPE_REFERENCE)) {
-            triggerType = context.getMergedJobDataMap().getString(SchedulerServiceConstants.TRIGGER_TYPE_REFERENCE);
-        }
-        if (triggerType == SchedulerServiceConstants.TRIGGER_TYPE_CRON && trigger.getNextFireTime() != null
-                && trigger.getNextFireTime().after(scheduledJobDetails.getNextRunTime())) {
-            scheduledJobDetails.updateNextRunTime(trigger.getNextFireTime());
-        }
+					}
+				}
+			}
+			final Long version = this.schedularService.fetchMaxVersionBy(jobKey) + 1;
+			String status = SchedulerServiceConstants.STATUS_SUCCESS;
+			String errorMessage = null;
+			String errorLog = null;
+			if (jobException != null) {
+				status = SchedulerServiceConstants.STATUS_FAILED;
+				this.stackTraceLevel = 0;
+				final Throwable throwable = getCauseFromException(jobException);
+				this.stackTraceLevel = 0;
+				StackTraceElement[] stackTraceElements = null;
+				errorMessage = throwable.getMessage();
+				stackTraceElements = throwable.getStackTrace();
+				final StringBuffer sb = new StringBuffer(throwable.toString());
+				for (final StackTraceElement element : stackTraceElements) {
+					sb.append("\n \t at ").append(element.getClassName()).append(".").append(element.getMethodName())
+							.append("(").append(element.getLineNumber()).append(")");
+				}
+				errorLog = sb.toString();
 
-        scheduledJobDetails.updatePreviousRunStartTime(context.getFireTime());
-        scheduledJobDetails.updateCurrentlyRunningStatus(false);
+			}
+			String triggerType = SchedulerServiceConstants.TRIGGER_TYPE_CRON;
+			if (context.getMergedJobDataMap().containsKey(SchedulerServiceConstants.TRIGGER_TYPE_REFERENCE)) {
+				triggerType = context.getMergedJobDataMap().getString(SchedulerServiceConstants.TRIGGER_TYPE_REFERENCE);
+			}
+			if (triggerType == SchedulerServiceConstants.TRIGGER_TYPE_CRON && trigger.getNextFireTime() != null
+					&& trigger.getNextFireTime().after(scheduledJobDetails.getNextRunTime())) {
+				scheduledJobDetails.updateNextRunTime(trigger.getNextFireTime());
+			}
 
-        final ScheduledJobRunHistory runHistory = new ScheduledJobRunHistory(scheduledJobDetails, version, context.getFireTime(),
-                new Date(), status, errorMessage, triggerType, errorLog);
-        // scheduledJobDetails.addRunHistory(runHistory);
+			scheduledJobDetails.updatePreviousRunStartTime(context.getFireTime());
+			scheduledJobDetails.updateCurrentlyRunningStatus(false);
 
-        this.schedularService.saveOrUpdate(scheduledJobDetails, runHistory);
+			final ScheduledJobRunHistory runHistory = new ScheduledJobRunHistory(scheduledJobDetails, version,
+					context.getFireTime(), new Date(), status, errorMessage, triggerType, errorLog);
+			// scheduledJobDetails.addRunHistory(runHistory);
 
-    }
+			this.schedularService.saveOrUpdate(scheduledJobDetails, runHistory);
+		} catch (Exception e) {
+			logger.debug("failed in jobWasExecuted for the job :" + context.getJobDetail().getKey().getName()
+					+ "with error :" + e.getMessage());
+		}
+	}
 
     private Throwable getCauseFromException(final Throwable exception) {
         if (this.stackTraceLevel <= SchedulerServiceConstants.STACK_TRACE_LEVEL
