868f7323c423cac0d9380a11020cba77d7708f3a
jinjurajan <jinju.rajan@confluxtechnologies.com>
2016-11-15 14:16:28 +0530

    2247: Adding client identifier status as enum value in intgration test

--- fineract-provider/src/integrationTest/java/org/apache/fineract/integrationtests/BorrowerAuthenticationDuringDisbursementTest.java
+++ fineract-provider/src/integrationTest/java/org/apache/fineract/integrationtests/BorrowerAuthenticationDuringDisbursementTest.java
@@ -127,7 +127,7 @@ public class BorrowerAuthenticationDuringDisbursementTest {
 		System.out.println("the client id is" + clientId);
 		String aadhaarNumber = "594671960" + generate(3);
 		Integer resourceId = (Integer) ClientHelper.addClientIdentifier(requestSpec, responseSpec, clientId,
-				aadhaarCodeValueId, aadhaarNumber, "Active");
+				aadhaarCodeValueId, aadhaarNumber, ClientHelper.CLIENT_IDENTIFIER_STATAUS_ACTIVE);
 		System.out.println("the client id and resource id are " + clientId + " re " + resourceId);
 		Assert.assertNotNull(resourceId);
 		System.out
