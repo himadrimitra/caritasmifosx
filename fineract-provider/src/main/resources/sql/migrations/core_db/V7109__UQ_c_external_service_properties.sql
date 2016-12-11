ef5ffb6cdb38b8fdc274ce79b58a8f39150d047d
satish-conflux <satish.sajjan@confluxtechnologies.com>
2016-12-12 01:57:04 +0530

    bugs_in_workflow

--- fineract-provider/src/main/resources/sql/migrations/core_db/V7109__UQ_c_external_service_properties.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7109__UQ_c_external_service_properties.sql
@@ -35,3 +35,247 @@ VALUES
 ('RELATIONSHIP_TYPE_K12_SON_IN_LAW', null, (SELECT es.id FROM c_external_service es WHERE es.name = 'HIGHMARK')),
 ('RELATIONSHIP_TYPE_K13_BROTHER_IN_LAW', null, (SELECT es.id FROM c_external_service es WHERE es.name = 'HIGHMARK')),
 ('RELATIONSHIP_TYPE_K15_OTHER', null, (SELECT es.id FROM c_external_service es WHERE es.name = 'HIGHMARK'));
+
+INSERT IGNORE INTO `m_code_value` (`code_id`, `code_value`, `code_description`, `order_position`, `code_score`, `is_active`, `is_mandatory`, `parent_id`)
+VALUES
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Gender'), 'Male', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Gender'), 'Female', NULL, 2, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Customer Identifier'), 'Voter Id', NULL, 2, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Mother', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Husband', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Son', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Daughter', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Wife', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Brother', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Mother in law', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Brother in law', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Daughter in law', NULL, 1, NULL, 1, 0, NULL),
+((SELECT c.id FROM m_code c WHERE c.code_name = 'Relationship'), 'Other', NULL, 1, NULL, 1, 0, NULL);
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Residential Address' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'AddressType'))
+WHERE esp.name = 'ADDRESS_TYPE_D01_RESIDENCE' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Permanent Address' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'AddressType'))
+WHERE esp.name = 'ADDRESS_TYPE_D04_PERMANENT' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Passport' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Customer Identifier'))
+WHERE esp.name = 'DOCUMENT_TYPE_ID01_PASSPORT' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Voter Id' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Customer Identifier'))
+WHERE esp.name = 'DOCUMENT_TYPE_ID02_VOTER_ID' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Male' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Gender'))
+WHERE esp.name = 'GENDER_TYPE_MALE' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Female' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Gender'))
+WHERE esp.name = 'GENDER_TYPE_FEMALE' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Father' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K01_FATHER' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Mother' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K03_MOTHER' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Husband' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K02_HUSBAND' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Son' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K04_SON' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Daughter' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K05_DAUGHTER' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Wife' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K06_WIFE' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Brother' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K07_BROTHER' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Mother in law' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K08_MOTHER_IN_LAW' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'father-in-law' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K09_FATHER_IN_LAW' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Daughter in law' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K10_DAUGHTER_IN_LAW' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Brother in law' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K13_BROTHER_IN_LAW' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Other' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_K15_OTHER' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
+
+UPDATE c_external_service_properties esp SET esp.value = (
+SELECT cv.id
+FROM m_code_value cv
+WHERE cv.code_value = 'Spouse' AND cv.code_id = (
+SELECT c.id
+FROM m_code c
+WHERE c.code_name = 'Relationship'))
+WHERE esp.name = 'RELATIONSHIP_TYPE_SPOUSE' AND esp.external_service_id = (
+SELECT es.id
+FROM c_external_service es
+WHERE es.name = 'HIGHMARK');
