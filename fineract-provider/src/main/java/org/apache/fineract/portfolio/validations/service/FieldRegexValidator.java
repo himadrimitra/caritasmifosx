1a292f7dfff371a1a5cf2192aee8d7dc2487d40c
jinjurajan <jinju.rajan@confluxtechnologies.com>
2016-12-30 15:35:39 +0530

    3028: regex validation fixes

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/validations/service/FieldRegexValidator.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/validations/service/FieldRegexValidator.java
@@ -10,23 +10,32 @@ import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.ApiParameterError;
 import org.apache.fineract.infrastructure.core.data.DataValidatorBuilder;
 import org.apache.fineract.infrastructure.core.exception.PlatformApiDataValidationException;
-
+import org.apache.fineract.infrastructure.core.serialization.FromJsonHelper;
+import org.apache.fineract.portfolio.client.api.ClientApiConstants;
 import org.apache.fineract.portfolio.validations.data.EntityFieldRegexValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.google.gson.JsonElement;
 
 @Component
 public class FieldRegexValidator {
 
 	private final EntityFieldRegexValidationReadPlatfromService entityFieldTypeReadflatformService;
+	private final FromJsonHelper fromApiJsonHelper;
 
 	@Autowired
-	public FieldRegexValidator(EntityFieldRegexValidationReadPlatfromService entityFieldTypeReadflatformService) {
+	public FieldRegexValidator(EntityFieldRegexValidationReadPlatfromService entityFieldTypeReadflatformService,
+			final FromJsonHelper fromApiJsonHelper) {
 		this.entityFieldTypeReadflatformService = entityFieldTypeReadflatformService;
+		this.fromApiJsonHelper = fromApiJsonHelper;
 	}
 
 	@SuppressWarnings("unused")
 	public void validateForFieldName(Integer entityType, final JsonCommand command) {
+		
+		final String json = command.json();
+		final JsonElement element = this.fromApiJsonHelper.parse(json);
 
 		final Collection<EntityFieldRegexValidation> retrieveEntityFieldType = entityFieldTypeReadflatformService
 				.retrieveEntityFieldType(entityType);
@@ -36,18 +45,21 @@ public class FieldRegexValidator {
 			EntityFieldRegexValidation en = (EntityFieldRegexValidation) it.next();
 			String regex = en.getRegex();
 			String errorMsg = en.getErrorMsg();
-			final String fieldName = command.stringValueOfParameterNamed(en.getFieldName());
+			String fieldName = null;
+			if(this.fromApiJsonHelper.parameterExists(en.getFieldName(), element)){
+			fieldName = command.stringValueOfParameterNamed(en.getFieldName());
+			}
 			if (en.getPreriquisitesType() != null) {
 				String preriquisitesType = en.getPreriquisitesType();
 				String[] parts = preriquisitesType.split(":");
 				final String documentTypeId = command.stringValueOfParameterNamed(parts[0]);
 				String preriquisitesTypeId = parts[1];
 				if (documentTypeId != null && preriquisitesTypeId != null && fieldName != null
-						&& documentTypeId.equals(preriquisitesTypeId)) {
+						&& !fieldName.equals("")  && documentTypeId.equals(preriquisitesTypeId)) {
 					validateForFieldName(regex, fieldName, errorMsg);
 				}
 
-			} else if (fieldName != null) {
+			} else if (fieldName != null && !fieldName.equals("")) {
 				validateForFieldName(regex, fieldName, errorMsg);
 			}
 		}
