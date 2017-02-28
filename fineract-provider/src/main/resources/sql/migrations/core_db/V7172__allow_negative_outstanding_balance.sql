b3f7f03d65f1c69bbdac4441fc0406dc6fa46494
pramod <pramod@confluxtechnologies.com>
2017-02-28 22:14:35 +0530

    3742:fixes for tranche loan with negative outstanding balances

--- fineract-provider/src/main/resources/sql/migrations/core_db/V7172__allow_negative_outstanding_balance.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7172__allow_negative_outstanding_balance.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `m_product_loan`
 	ADD COLUMN `allow_negative_loan_balances` TINYINT(1) NOT NULL DEFAULT '0',
-	ADD COLUMN `consider_future_disbursements_in_schedule` TINYINT(1) NOT NULL DEFAULT '0' 
+	ADD COLUMN `consider_future_disbursements_in_schedule` TINYINT(1) NOT NULL DEFAULT '0',
 	ADD COLUMN `consider_all_disbursements_in_schedule` TINYINT(1) NOT NULL DEFAULT '0';
 	
 ALTER TABLE `m_loan`
