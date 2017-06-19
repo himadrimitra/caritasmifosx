9dcb08454150bb20ed47abc50fd44b4897aced86
himadri <himadri.mitra@confluxtechnologies.com>
2017-06-19 13:54:07 +0530

    FINFLUX-817_1:Migration script no-7233 was failing on develop branch

--- fineract-provider/src/main/resources/sql/migrations/core_db/V7233__Adding_new_column(system_user)_in_m_appuser.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V7233__Adding_new_column(system_user)_in_m_appuser.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `m_appuser`
-ADD COLUMN `system_user` BIT(1) NOT NULL DEFAULT b'0' AFTER `failed_login_attempt`;
-update  m_appuser u 
-set u.system_user = 1
-where u.username = 'system';
+	ADD COLUMN `system_user` TINYINT(1) NOT NULL DEFAULT 0 AFTER `failed_login_attempt`;
+	
+UPDATE `m_appuser` SET `system_user`= 1 WHERE username = 'system';
