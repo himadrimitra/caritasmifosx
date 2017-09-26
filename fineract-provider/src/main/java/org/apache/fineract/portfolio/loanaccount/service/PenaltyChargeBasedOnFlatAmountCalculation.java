d8f5054229103362b802d9719637eae95bbb9c31
pramod <pramod@confluxtechnologies.com>
2017-09-26 18:58:09 +0530

    FINFLUX-2351,FINFLUX-2353 : Penalty charge fixes

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/PenaltyChargeBasedOnFlatAmountCalculation.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/PenaltyChargeBasedOnFlatAmountCalculation.java
@@ -2,14 +2,13 @@ package org.apache.fineract.portfolio.loanaccount.service;
 
 import java.math.BigDecimal;
 
-import org.apache.fineract.organisation.monetary.domain.Money;
 import org.apache.fineract.portfolio.loanaccount.loanschedule.domain.PenaltyPeriod;
 
 public class PenaltyChargeBasedOnFlatAmountCalculation implements PenaltyChargeCalculator {
 
     @Override
-    public Money calculateCharge(PenaltyPeriod penaltyPeriod) {
-        return Money.of(penaltyPeriod.getOutstanding().getCurrency(), new BigDecimal(penaltyPeriod.getPercentageOrAmount()));
+    public BigDecimal calculateCharge(PenaltyPeriod penaltyPeriod) {
+        return  new BigDecimal(penaltyPeriod.getPercentageOrAmount() * penaltyPeriod.getNumberofTimes());
     }
 
 }
