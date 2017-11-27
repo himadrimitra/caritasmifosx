63f007ec7f5d53b8016e7345a837ed38155c9218
pramod sharma <pramod.sharma@confluxtechnologies.com>
2017-11-27 16:44:54 +0530

    release amount

--- fineract-provider/src/main/java/com/finflux/portfolio/investmenttracker/domain/InvestmentAccountSavingsLinkagesRepositoryWrapper.java
+++ fineract-provider/src/main/java/com/finflux/portfolio/investmenttracker/domain/InvestmentAccountSavingsLinkagesRepositoryWrapper.java
@@ -1,9 +1,10 @@
 package com.finflux.portfolio.investmenttracker.domain;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.finflux.portfolio.investmenttracker.Exception.InvestmentAccountNotFoundException;
 import com.finflux.portfolio.investmenttracker.Exception.InvestmentAccountSavingsLinkagesNotFoundException;
 
 @Service
@@ -20,6 +21,19 @@ public class InvestmentAccountSavingsLinkagesRepositoryWrapper{
         final InvestmentAccountSavingsLinkages investmentSavingAccount = this.repository.findOne(id);
         if (investmentSavingAccount == null) { throw new InvestmentAccountSavingsLinkagesNotFoundException(id); }
         return investmentSavingAccount;
+    }
+    
+
+    public void save(final InvestmentAccountSavingsLinkages investmentAccountSavingsLinkages) {
+        this.repository.save(investmentAccountSavingsLinkages);
+    }
+
+    public void save(final Collection<InvestmentAccountSavingsLinkages> investmentAccountSavingsLinkages) {
+        this.repository.save(investmentAccountSavingsLinkages);
+    }
+
+    public void delete(final InvestmentAccountSavingsLinkages investmentAccountSavingsLinkages) {
+        this.repository.delete(investmentAccountSavingsLinkages);
     }
 
 }
